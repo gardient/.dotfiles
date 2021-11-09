9d843a1efdea4aacbe6270b386bd5e7f07d1fed2
Gyula Kerezsi <gardient@users.noreply.github.com>
2021-11-09 12:03:33 +0200

    added Vundle

---
+++ scripts/40-install-vundle.sh
@@ -0,0 +1,16 @@
+if [[ ! -d "~/.vim/bundle/Vundle.vim" || -n $UPDATE ]]; then
+  info "starting Vundle installation"
+  if [ -d "~/.vim/bundle/Vundle.vim" ]; then
+    debug "Vundle already cloned. pulling"
+    pushd ~/.vim/bundle/Vundle.vim > $output
+    git pull > $output
+    popd > $output
+  else
+    debug "cloning Vundle repo from https://github.com/VundleVim/Vundle.vim.git"
+    git clone --depth 1 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim > $output
+  fi
+  vim +PluginInstall +qall
+  success "Vundle installed"
+else
+  success "Vundle already installed skipping (add the UPDATE env variable if you want to force update)"
+fi
