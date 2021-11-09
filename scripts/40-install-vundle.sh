356cda98af7839afaedbb07ba8df7e6700bcf9cd
Gardient <gardient@users.noreply.github.com>
2021-11-09 18:50:48 +0200

    some fixes

--- scripts/40-install-vundle.sh
+++ scripts/40-install-vundle.sh
@@ -1,12 +1,13 @@
-if [[ ! -d "~/.vim/bundle/Vundle.vim" || -n $UPDATE ]]; then
+if [[  ( ! -d "$HOME/.vim/bundle/Vundle.vim" ) || -n $UPDATE ]]; then
   info "starting Vundle installation"
-  if [ -d "~/.vim/bundle/Vundle.vim" ]; then
+  if [ -d "$HOME/.vim/bundle/Vundle.vim" ]; then
     debug "Vundle already cloned. pulling"
     pushd ~/.vim/bundle/Vundle.vim > $output
     git pull > $output
     popd > $output
   else
     debug "cloning Vundle repo from https://github.com/VundleVim/Vundle.vim.git"
+    mkdir -p ~/.vim/bundle
     git clone --depth 1 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim > $output
   fi
   vim +PluginInstall +qall
