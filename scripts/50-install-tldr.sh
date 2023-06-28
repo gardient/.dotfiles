96de489074d72c360db39511e44dd6b331bc3594
Gyula Kerezsi <gardient@users.noreply.github.com>
2023-06-28 09:54:45 +0300

    added tldr pages to the install list

---
+++ scripts/50-install-tldr.sh
@@ -0,0 +1,14 @@
+if [[ -z $(type -P tldr) || -n $UPDATE ]]; then
+  info "Installing tldr-pages/tldr"
+
+  debug "updating apt"
+  sudo apt update > $output
+  debug "installing python stuff"
+  sudo apt install python3-dev python3-pip python3-setuptools -y > $output
+  debug "pip installing tldr"
+  pip3 install tldr --user > $output
+
+  success "thefuck installed"
+else
+  success "tldr-pages/tldr already installed skipping (add the UPDATE env variable if you want to force update)"
+fi
