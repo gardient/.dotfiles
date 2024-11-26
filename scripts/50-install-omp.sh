043deace06d37c99359a528538df72e971a51298
gardient <gardient@users.noreply.github.com>
2024-11-26 22:42:56 +0200

    change to omp

---
+++ scripts/50-install-omp.sh
@@ -0,0 +1,10 @@
+if [[ -z $POSH_THEME || -n $UPDATE ]]; then
+  info "starting posh installation"
+
+  debug "getting posh install script"
+  curl -s https://ohmyposh.dev/install.sh | bash -s
+
+  success "posh installed"
+else
+  success "posh already installed skipping (add the UPDATE env variable if you want to force update)"
+fi
